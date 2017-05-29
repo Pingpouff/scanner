2d02c50ff4678b64b65ae5de97c7736701869ffc
pingpouff <pingpouff@gmail.com>
2017-05-29 23:43:58 +0200

    new tree scanner added

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('TEST!!!');
+var path = require("path");
+var scanner = require("./src/scanner");
+var pathArgument = path.resolve(process.argv[2]);
+const jetpack = require('fs-jetpack');
+// console.log(jetpack.inspectTree(pathArgument));
+var tree = { name: 'root'};
+scanner.scan(pathArgument, tree);
+console.log(tree);
+// jetpack.inspectTreeAsync(pathArgument, { relativePath: true })
+// .then(tree => {
+//     console.log(tree);
+// })
+// .catch(error => {
+//     console.log(error);
+// });
---
+++ src/scanner.js
@@ -0,0 +1,30 @@
+
+const scan = (pathArgument, tree) => {
+	var fs = require("fs");
+	var path = require("path");
+	// TODO use async call + promise
+	tree.children = [];
+	fs.readdirSync(pathArgument).forEach(function (file) {
+		var currentPath = path.join(pathArgument, file);
+		const isCharSupported = currentPath.indexOf('é') === -1
+			&& currentPath.indexOf('ê') === -1
+			&& currentPath.indexOf('ï') === -1
+			&& currentPath.indexOf('à') === -1
+			&& currentPath.indexOf('è') === -1
+			&& currentPath.indexOf('æ') === -1
+			&& currentPath.indexOf('ô') === -1
+			&& currentPath.indexOf('É') === -1
+			&& currentPath.indexOf('œ') === -1
+			&& currentPath.indexOf('á') === -1;
+		if (!isCharSupported) console.warn('unsupported path: ' + currentPath);
+		var dir = { name: file };
+		tree.children.push(dir);
+		if (isCharSupported && fs.lstatSync(currentPath).isDirectory()) {
+			scan(currentPath, dir);
+		}
+	});
+}
+
+var Scanner = module.exports = {
+	scan
+}

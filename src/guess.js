c84b36d1e05ec70e420221ebd8e2d27ec8a1ba29
pingpouff <pingpouff@gmail.com>
2017-06-16 18:14:02 +0200

    added guess no matching case

--- src/guess.js
+++ src/guess.js
@@ -2,12 +2,16 @@ const guess = {};
 const regex = '((?:\.?\\w)*)\.[Ss]0*(\\d*)?[Ee]0*(\\d+)';
 
 guess.what = fileName => {
-    console.log(regex); // TODO could this be done with deep learning?
+    console.log(fileName);
     const matches = fileName.match(regex);
-    const name = matches[1];
-    const season = matches[2];
-    const episode = matches[3];
-    return {name, season, episode};
+    if (matches) {
+        const name = matches[1];
+        const season = matches[2];
+        const episode = matches[3];
+        return { name, season, episode };
+    } else {
+        return {};
+    }
 };
 
 module.exports = guess;
